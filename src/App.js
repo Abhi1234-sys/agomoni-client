ce9d54be71d0412f3d3bbe381592aea939248b18
abhimanyu mahato <abhimanyumahato19.bqa@gmail.com>
2026-09-23 20:41:28 +0530

    Removed AlponaTrail component to fix mobile lag

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Dhaki from "./components/Dhaki";
 import Footer from "./components/Footer";
 import ShiuliShower from "./components/ShiuliShower";
 import KashfulProgressBar from "./components/KashfulProgressBar";
-import AlponaTrail from "./components/AlponaTrail";
+
 
 // Pages import
 import Home from "./pages/Home";
@@ -102,8 +102,7 @@ function App() {
   return (
     <BrowserRouter>
       <div className="app">
-        {/* ALPONA CURSOR TRAIL */}
-        <AlponaTrail />
+     
 
         {/* KASHFUL SCROLL PROGRESS BAR */}
         <KashfulProgressBar />
